3825a2124d48ce11aac1cfa9d4a69af9c265db51
shota.hanaoka <ec2-user@ip-172-31-11-154.us-east-2.compute.internal>
2018-06-18 06:03:29 +0000

    ranking

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -23,3 +23,4 @@ class WelcomeController extends Controller
         ]);
     }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
@@ -28,6 +31,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('have', 'ItemUserController@have')->name('item_user.have');
     Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
+    
+    
 });
 
 
